faa5f96ac16cdafa8945318551891e88060d6125
henry.bobka <henry.bobka@gofore.com>
2024-04-05 17:00:56 +0200

    render category content

--- fe/src/views/DetailsView.vue
+++ fe/src/views/DetailsView.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+import type { TileCategory } from '@/components/TileItem.vue'
+import { useInventory } from '@/composables/useInventory'
+import { computed, onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const { stateInventory } = useInventory()
+
+const category = ref<TileCategory | null>(null)
+onMounted(() => {
+  if (route.query.category) {
+    category.value = route.query.category as TileCategory
+  }
+})
+
+const inventoryItems = computed(() => {
+  return category.value && stateInventory.value
+    ? stateInventory.value[category.value as keyof typeof stateInventory.value]
+    : []
+})
+</script>
+
 <template>
   <main>
     <h1>This is The Details View</h1>
+    <ul v-if="inventoryItems">
+      <li v-for="(item, index) in inventoryItems" :key="index">{{ item }}</li>
+    </ul>
   </main>
 </template>
