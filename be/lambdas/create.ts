5d2263bf19f121e3c9abc14915daa88230c95c3c
henry.bobka <henry.bobka@gofore.com>
2024-05-04 14:32:00 +0200

    adjust creation/adding of new items

--- be/lambdas/create.ts
+++ be/lambdas/create.ts
@@ -1,28 +1,38 @@
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import { DynamoDB } from "@aws-sdk/client-dynamodb";
-import { v4 as uuidv4 } from "uuid";
 
 const TABLE_NAME = process.env.TABLE_NAME || "";
 const PRIMARY_KEY = process.env.PRIMARY_KEY || "";
 
 const db = DynamoDBDocument.from(new DynamoDB());
 
-const RESERVED_RESPONSE = `Error: You're using AWS reserved keywords as attributes`,
-  DYNAMODB_EXECUTION_ERROR = `Error: Execution update, caused a Dynamodb error, please take a look at your CloudWatch Logs.`;
-
 export const handler = async (event: any = {}): Promise<any> => {
   if (!event.body) {
-    return { statusCode: 400, body: "invalid request, you are missing the parameter body" };
+    return { statusCode: 400, body: "no body" };
   }
+
   const item = typeof event.body == "object" ? event.body : JSON.parse(event.body);
-  item[PRIMARY_KEY] = uuidv4();
-  const params = {
+  const itemId = item.userEmail;
+  const category = item.category;
+  const newItem = item.item;
+
+  let inventory = await getCurrentInventory(itemId);
+
+  if (!inventory[category]) {
+    inventory[category] = [];
+  }
+  inventory[category].push(newItem);
+
+  const putParams = {
     TableName: TABLE_NAME,
-    Item: item,
+    Item: {
+      [PRIMARY_KEY]: itemId,
+      inventory,
+    },
   };
 
   try {
-    await db.put(params);
+    await db.put(putParams);
     return {
       statusCode: 201,
       headers: {
@@ -32,10 +42,22 @@ export const handler = async (event: any = {}): Promise<any> => {
       body: "",
     };
   } catch (dbError: any) {
-    const errorResponse =
-      dbError.code === "ValidationException" && dbError.message.includes("reserved keyword")
-        ? RESERVED_RESPONSE
-        : DYNAMODB_EXECUTION_ERROR;
-    return { statusCode: 500, body: errorResponse };
+    return { statusCode: 500, body: JSON.stringify(dbError) };
+  }
+};
+
+const getCurrentInventory = async (itemId: string) => {
+  const params = {
+    TableName: TABLE_NAME,
+    Key: {
+      [PRIMARY_KEY]: itemId,
+    },
+  };
+
+  try {
+    const response = await db.get(params);
+    return response.Item?.inventory || {};
+  } catch (dbError: any) {
+    return { statusCode: 500, body: dbError };
   }
 };
