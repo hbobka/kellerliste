fc26e570f1f4d4df782eb8f2d1637c747ebe16d5
henry.bobka <henry.bobka@gofore.com>
2024-05-04 14:26:54 +0200

    add loading spinner

--- fe/src/views/AuthView.vue
+++ fe/src/views/AuthView.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
 import { useAuth } from '@/composables/useAuth'
 import router from '@/router'
 
 const route = useRoute()
 const { setAuthCode, getAuthTokens, getUserInfo } = useAuth()
+const isLoading = ref(false)
 
 onMounted(async () => {
   const authCode = route.query?.code?.toString()
   if (authCode) {
     setAuthCode(authCode)
+    isLoading.value = true
     const tokens = await getAuthTokens(authCode)
 
     if (tokens?.accessToken) {
       await getUserInfo(tokens?.accessToken)
+      isLoading.value = false
       router.push({ name: 'home' })
     }
   }
@@ -23,26 +26,33 @@ onMounted(async () => {
 
 <template>
   <main>
-    <h2>Processing Login ...</h2>
+    <div class="kl-progress" v-if="isLoading">
+      <v-progress-circular
+        :size="100"
+        :width="8"
+        color="kl-black"
+        indeterminate
+      ></v-progress-circular>
+      <p>Authentication in progress</p>
+    </div>
   </main>
 </template>
 
 <style scoped>
 main {
   position: absolute;
-  top: 30%;
+  top: 40%;
   left: 50%;
-  transform: translate(-50%, -30%);
-  width: 20rem;
-  margin: 0 auto;
+  transform: translate(-50%, -40%);
 }
 
-h2 {
-  text-align: center;
-}
-
-form {
+.kl-progress {
   display: flex;
   flex-direction: column;
+  align-items: center;
+}
+
+.kl-progress p {
+  margin-top: 1rem;
 }
 </style>
