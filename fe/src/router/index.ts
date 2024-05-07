9cc089ea726cef18b111dc950271de452d47f997
henry.bobka <henry.bobka@gofore.com>
2024-05-07 15:23:29 +0200

    cleanup

--- fe/src/router/index.ts
+++ fe/src/router/index.ts
@@ -3,7 +3,7 @@ import { useAuth } from '@/composables/useAuth'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
