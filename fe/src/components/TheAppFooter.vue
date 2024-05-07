878a2504bd199d6a9794c96955a62fee250d6aaf
henry.bobka <henry.bobka@gofore.com>
2024-05-07 15:31:29 +0200

    add footer

---
+++ fe/src/components/TheAppFooter.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useDate } from 'vuetify'
+
+const date = useDate()
+const formattedDate = date.format(Date.now(), 'fullDateTime')
+</script>
+<template>
+  <footer>
+    <p>{{ formattedDate }}</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 1rem;
+}
+
+footer p {
+  text-align: center;
+}
+</style>
